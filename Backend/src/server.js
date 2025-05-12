3eb95b5c45c28f09c3a98c51c649b4136b57e08c
DonatoDiCarlo404 <donato.dicarlo404@gmail.com>
2025-05-12 18:11:03 +0200

    collegamento Vercel

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 3001
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173',
+        'https://strive-blog-frontend-8pjhhu59j-donatodicarlos-projects.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
